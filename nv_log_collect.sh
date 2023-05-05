2fa54cc3f8f37065b9c23161e480675cc8249fcd
Andrej Semen <asemen2608@gmail.com>
2023-05-05 16:00:46 +0200

    Add files via upload

--- nv_log_collect.sh
+++ nv_log_collect.sh
@@ -2,16 +2,28 @@
 
 LANG="de_DE@euro iso-8859-15"
 DATE="`date +%Y%m%d%H%M`"
-NS=neuvector
+NS="neuvector"
+
+## create log dir
+DI=log_$DATE
+mkdir $DI
 
 # collecting log files
-kubectl -n $NS top nodes > top_nodes_$DATE.log
-kubectl -n $NS top pods > top_pods_$DATE.log
-kubectl -n $NS top pods | grep enforcer >  top_pods_enforcer_$DATE.log
-kubectl describe pods -n $NS > describe_pods._$DATE.log
+kubectl -n $NS top nodes > $DI/top_nodes_$DATE.log
+kubectl -n $NS top pods > $DI/top_pods_$DATE.log
+kubectl -n $NS top pods | grep enforcer >  $DI/top_pods_enforcer_$DATE.log
+#i ps aux
+for i in `kubectl get pods -n $NS | awk '{print $1}'` 
+    do
+      #echo $i
+      echo "kubectl -n $NS exec $i -- ps aux > $DI/ps_aux_pod_$i--$DATE.log"
+      kubectl -n $NS exec $i -- ps aux > $DI/ps_aux_pod_$i--$DATE.log
+    done
 
-for i in `kubectl get pods -n $NS | grep controller | awk '{print $1}'`;do kubectl logs $i -n $NS > $i.$DATE.log; done
-#for i in `kubectl get pods -n $NS | grep controller | awk '{print $1}'`;do kubectl logs $i -n $NS > $i_$DATE.log; done
+kubectl describe pods -n $NS > $DI/describe_pods._$DATE.log
+
+for i in `kubectl get pods -n $NS | grep controller | awk '{print $1}'`;do kubectl logs $i -n $NS > $DI/$i.$DATE.log; done
+#for i in `kubecSl get pods -n neuvector | grep controller | awk '{print $1}'`;do kubectl logs $i -n neuvector > $i_$DATE.log; done
 
 
 
